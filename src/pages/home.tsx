7a8e052c395456db0d47075f795c439290c49fa7
yuchin.huang <yuchin.huang@acercsi.com>
2025-05-09 00:42:31 +0800

    修改主頁的圖片路徑

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
+import exampleImg from '../assets/img/home.jpg';
 export const Home = () => {
   return (
     <div className="overflow-hidden flex items-center justify-center relative">
       {/* 背景圖 */}
       <img
-        src="./home.jpg" // ← 替換成你的圖片路徑
+        src={exampleImg} // ← 替換成你的圖片路徑
         alt="TMGM早安籃球俱樂部"
         className="max-w-full max-h-full object-contain"
       />
